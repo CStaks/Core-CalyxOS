58e7322c191cbced8eab537ec4a31f0811362f2c
Callen Flynn <ctflynn10@icloud.com>
2026-04-30 04:52:05 -0400

    Update apps.sh

--- src/apps.sh
+++ src/apps.sh
@@ -23,18 +23,24 @@ sudo apt autoremove -y
 
 # Install KDE Plasma
 echo "Installing KDE Plasma..."
-sudo apt install -y kde-plasma-desktop kde-plasma-workspace
+sudo apt install -y kde-plasma-desktop
 
 # Add Brave repo (+100000 aura)
 curl https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key add -
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 
-# install Brave and Kitty
-sudo apt install -y brave-browser kitty
+# Install Brave, Kitty, and other applications that are goated
+sudo apt install -y \
+  brave-browser \
+  kitty \
+  vlc \
+  gwenview \
+  btop \
+  libreoffice
 
 # Set defaults
 sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/brave-browser 100
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 100
 
-echo "✓ Apps installed"
+echo "Apps installed"
